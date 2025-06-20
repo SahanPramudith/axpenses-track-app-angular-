bec557747aa9f2c348f6d79eff20acc23c391555
sahanpramudith <pramuditsahan466@gmail.com>
2025-06-20 21:40:55 +0530

    feat: implement expense management service and model

---
+++ src/app/core/mdoel/expence.model.ts
@@ -0,0 +1,30 @@
+export interface Expenses {
+    id?: number;
+    amount: number;
+    description: string;
+    category: ExpenceCategory;
+    date: string;
+    paymentMethod: PaymentMethod;
+    tag?:string[];
+    note?:string
+}
+
+export  enum ExpenceCategory{
+    FOOD='food',
+    TRANSPORT='transport',
+    UTILITY='utility',
+    ENTERTAINMENT='entertainment',
+    Hopping='hopping',
+    HEALTH='health',
+    OTHER='other'
+}
+
+export enum PaymentMethod{
+    CASH='cash',
+    CREDIT_CARD='credit card',
+    DEBIT_CARD='debit card',
+    Bank_Transfer='bank transfer',
+    PAYPAL='paypal',
+    OTHER='other'
+}
+
---
+++ src/app/core/service/service.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Expenses } from '../mdoel/expence.model';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServiceService {
+
+  private readonly API_URL='http://localhost:3000/expence';
+
+  private http=inject(HttpClient);
+
+
+  getExpenses(){
+    return this.http.get<Expenses>(this.API_URL);
+  }
+
+  getExpensesById(id:number){
+    return this.http.get<Expenses>(`${this.API_URL}/${id}`)
+  }
+
+  crateExpense(expense:Omit<Expenses,'id'>){
+    return this.http.post(this.API_URL,expense);
+  }
+  updateExpense(id:number,expense:Expenses){
+    return this.http.put(`${this.API_URL}/${id}`,expense);
+  }
+  
+  deleteExpense(id:number){
+    return this.http.delete(`${this.API_URL}/${id}`);
+  }
+
+}
