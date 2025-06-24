3568280f345e0f164241967ac03a610e6cdf95ed
sahanpramudith <pramuditsahan466@gmail.com>
2025-06-24 12:13:18 +0530

    feat: implement expense list component with data fetching and table display

--- src/app/feture/expence/expencce-list/expencce-list.component.ts
+++ src/app/feture/expence/expencce-list/expencce-list.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { ServiceService } from '../../../core/service/service.service';
+import { Expenses } from '../../../core/mdoel/expence.model';
+import { TableModule } from 'primeng/table';
+import { CurrencyPipe, DatePipe } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+
 
 @Component({
   selector: 'app-expencce-list',
-  imports: [],
+  standalone: true,
+  imports: [TableModule,ButtonModule,CurrencyPipe,DatePipe], // Add CommonModule, etc. if needed
   templateUrl: './expencce-list.component.html',
   styleUrl: './expencce-list.component.css'
 })
-export class ExpencceListComponent {
+export class ExpencceListComponent implements OnInit {
 
+  private expensesList = inject(ServiceService);
+
+  // signal to hold list of expenses
+  expense = signal<Expenses[]>([]);
+
+  ngOnInit(): void {
+    this.expensesList.getExpenses().subscribe({
+      next: (expenses) => {
+        console.log(expenses);
+        this.expense.set(Array.isArray(expenses) ? expenses : [expenses]); 
+      },
+      error: (err) => {
+        console.error('Error fetching expenses:', err);
+      }
+    });
+  }
 }
